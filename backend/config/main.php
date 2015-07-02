6d1e2897107ec9f690a70f093a7d05a98b2f8236
lotysh.vm <Gangsta1@i.ua>
2015-07-02 09:32:44 +0300

    db component add

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,6 +11,20 @@ return [
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2',
+            //'dsn' => 'sqlite:/path/to/database/file', // SQLite
+            //'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase', // PostgreSQL
+            //'dsn' => 'cubrid:dbname=demodb;host=localhost;port=33000', // CUBRID
+            //'dsn' => 'sqlsrv:Server=localhost;Database=mydatabase', // MS SQL Server, sqlsrv driver
+            //'dsn' => 'dblib:host=localhost;dbname=mydatabase', // MS SQL Server, dblib driver
+            //'dsn' => 'mssql:host=localhost;dbname=mydatabase', // MS SQL Server, mssql driver
+            //'dsn' => 'oci:dbname=//localhost:1521/mydatabase', // Oracle
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
         'request' => [
             'class' => 'common\components\Request',
             'web' => '/backend/web',
